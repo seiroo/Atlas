02fa16a7bf3211cde67b01c8b8c3d9d088ed3d9a
seiro <jonatnaclavjo58@gmail.com>
2025-10-13 18:27:25 -0500

    finished interface

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from "./components/footer/footer";
+import { Header } from "./components/header/header";
+import { Home } from "./pages/home/home";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  imports: [Footer, Header, Home]
 })
 export class App {
   protected readonly title = signal('atlas');
